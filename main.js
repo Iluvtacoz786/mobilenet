62c229f2ed9b090fabd9a7ce9775e1318226abbe
Zoya Rana <74219818+Iluvtacoz786@users.noreply.github.com>
2021-06-22 17:37:17 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -8,6 +8,29 @@ Webcam.set({
 Webcam.attach("#camera");
 
 function capture(){
-    Webcam.snap
+    Webcam.snap(function(URI){
+        document.getElementById("snapshot").innerHTML="<img src='"+URI+"' id='result_img'>"
+    })
+}
+console.log(ml5.version);
+classifier=ml5.imageClassifier("MobileNet", modelLoaded)
 
+function modelLoaded(){
+    console.log("hi");
+}
+function identify(){
+img=document.getElementById("result_img");
+classifier.classify(img, gotResult);
+
+}
+function gotResult(error,result){
+    if(error){
+        console.log("error");
+    }
+    else{
+        console.log(result);
+        document.getElementById("value").innerHTML=result[0].label;
+
+
+    }
 }
